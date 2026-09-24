1216a0f837a9db6f12f09719860ccf570f9190a0
Brendan Rubilar <Brendanrubilarvivanco@gmail.com>
2026-09-24 15:42:50 -0300

    Test de animación simple

--- frontend/src/components/LemurLoader.tsx
+++ frontend/src/components/LemurLoader.tsx
@@ -1,18 +1,16 @@
 function LemurLoader() {
   return (
     <section className="lemur-loader">
-
       <div className="lemur-message">
         <span>🧠</span>
         Preparando tu desafío...
       </div>
 
       <div className="lemur-track">
-        <div className="lemur">
-          🐒
+        <div className="lemur-walker">
+          <div className="lemur-sprite" />
         </div>
       </div>
-
     </section>
   )
 }
